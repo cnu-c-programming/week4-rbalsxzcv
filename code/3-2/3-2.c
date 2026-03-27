a98bce2d38bc244565409b1c1438c3c3cf21c269
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-03-27 15:15:47 +0900

    Update 3-2.c

---
+++ code/3-2/3-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap_endian(int *x){
+    unsigned char* addr = (unsigned char*)x;
+    unsigned char* sto;
+    *sto = *addr;
+    *addr = *(addr+3);
+    *(addr+3) = *sto;
+    *sto = *(addr+1);
+    *(addr+1) = *(addr+2);
+    *(addr+2) = *sto;
+}
+
+int main(){
+    int x = 0x12345678;
+
+    printf("%x\n", x);
+    swap_endian(&x);
+    printf("%x\n",x);
+
+    return 0;
+}
